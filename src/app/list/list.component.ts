cc2c51c0651d81f394f583144283fa03c17628f1
Damian Kędra <dkedra@esus-it.com>
2024-12-15 18:14:04 +0100

    input list

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
 
 @Component({
-  selector: 'app-list',
-  imports: [],
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+    selector: 'app-list',
+    imports: [
+        FormsModule,
+        NgForOf
+    ],
+    templateUrl: './list.component.html',
+    styleUrl: './list.component.css'
 })
 export class ListComponent {
+    inputValue: string = '';
+    list: string[] = [];
 
+    addToList(): void {
+        if (this.inputValue) {
+            this.list.push(this.inputValue)
+            this.inputValue = ''
+        }
+    }
+
+    remove(index: number): void {
+        this.list.splice(index, 1)
+    }
 }
--- src/app/random/random.component.ts
+++ src/app/random/random.component.ts
@@ -11,17 +11,15 @@ import {NgClass} from "@angular/common";
   styleUrl: './random.component.css'
 })
 export class RandomComponent implements OnInit {
-    @Input() max: number = 100
-    randomNumber: number;
-    isSmaller: boolean;
+    @Input() max: number = 0;
+    randomNumber: number = 0;
+    isSmaller: boolean = false;
 
-    constructor(private randomService: RandomService) {
-        this.randomNumber = this.randomService.generateRandom(this.max)
-        this.isSmaller = this.randomService.numberIsSmaller(this.randomNumber, this.max)
-    }
+    constructor(private randomService: RandomService) {}
 
     ngOnInit(): void {
-
+        this.randomNumber = this.randomService.generateRandom(this.max)
+        this.isSmaller = this.randomService.numberIsSmaller(this.randomNumber, this.max)
     }
 
     generateNumer(): void {
